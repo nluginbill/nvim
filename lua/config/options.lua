165f85228c70b07dab70c30f0ee2fd0dad2da1a8
Kirby Little <krbylit@users.noreply.github.com>
2023-11-15 09:52:25 -0600

    spaces to tabs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.tabstop = 4 -- A tab is equal to 4 spaces
+vim.opt.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent
+vim.opt.expandtab = true -- Convert tabs to spaces
